fecca00b32e612e9c0d4ea99bad60dd3fb5e6eaa
fausto_gomez <fausto.gomez.pelaez@gmail.com>
2026-02-16 13:54:36 -0500

    Se cambia la conexión de la BD a connectionString

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,13 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  connectionString: process.env.DATABASE_URL
 });
 
+pool.query('SELECT 1')
+.then(() => console.log('Neon Conectado correctamente'))
+.catch(err => console.error('Error Neon', err.message)
+);
 pool.on('connect', () => {
   console.log('🟢 Conectado a PostgreSQL');
 });
